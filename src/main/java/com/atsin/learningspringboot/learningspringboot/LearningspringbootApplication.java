608949c47da9d7f12c35f8900e1c44cc090c56cb
atsin6 <66594774+atsin6@users.noreply.github.com>
2024-08-09 18:01:05 +0530

    Chapter 2.1

---
+++ src/main/java/com/atsin/learningspringboot/learningspringboot/LearningspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.atsin.learningspringboot.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsin/learningspringboot/learningspringboot/LearningspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsin.learningspringboot.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
